08cec87907f9930de37e82a2aa0e38841f181366
BerkahEsport <berkahesport@gmail.com>
2024-11-13 11:15:17 +0700

    Update

--- messages/message_upsert.js
+++ messages/message_upsert.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import cheerio from "cheerio";
+import * as cheerio from "cheerio";
 export default async function message_upsert(sock, m, store, commands, config, functions) {
 	// Self mode on if you want.
 	// if (m.sender.split("@")[0] === config.number.owner) return;

adc88fdeb479da549b5e2094ed74e02affc14f69
BerkahEsport <berkahesport@gmail.com>
2025-01-22 22:20:11 +0700

    Add and update feature!

---
+++ commands/games/dadu.js
@@ -0,0 +1,45 @@
+/*<============== CREDITS ==============>
+        Author: berkahesport
+        Github: https://github.com/BerkahEsport/
+        Contact me: 62895375950107
+
+        Do not delete the source code.
+        It is prohibited to
+        sell and buy scripts
+        without the knowledge
+        of the script owner.
+
+        Thank you to Allah S.W.T
+<============== CREDITS ==============>*/
+
+export default {
+    name: "dadu",
+    command: ["dadu", "helpao"],
+    tags: "games",
+    desc: "Guess game dadu.",
+    customPrefix: "",
+    example: "",
+    limit: false,
+    isOwner: false,
+    isPremium: false,
+    isBotAdmin: false,
+    isAdmin: false,
+    isGroup: false,
+    isPrivate: false,
+    run: async(m, {
+        functions,
+        args
+    }) => {
+        const result = await functions.api("api/dadu");
+        m.log(result)
+        const choice = parseInt(args[0]);
+        const reward = functions.randomInt(1, 100);
+        const fine = functions.randomInt(1, 100);
+        if (result.result.no === choice) {
+            await m.reply(result.result.url);
+            m.reply(`✅ *That's great*, your answer is correct.!\n\n🎉 *Reward*: ${reward} exp.`);
+        } else {
+            m.reply(`❌ *You are not lucky*, you get a fine of _${fine}_ exp.`)
+        }
+    }
+}
